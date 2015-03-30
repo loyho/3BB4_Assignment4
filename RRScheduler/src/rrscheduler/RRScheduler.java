a4122d7c0b32c2747e9151c8500a54862ec3f915
Jenell-M-Hogg <jenellmhogg@live.com>
2015-03-29 23:25:45 -0400

    Update RR Scheduler

--- RRScheduler/src/rrscheduler/RRScheduler.java
+++ RRScheduler/src/rrscheduler/RRScheduler.java
@@ -14,24 +14,13 @@ public class RRScheduler {
     
     //A time for thread to wait after printing a message (This is so the output is more human observer-friendly.
     public static int messageTime=500;
-    
-    
     public static ReadyQueue readyQueue;
     
-    public static GrimReaper reaper;
    
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
-        Random r=new Random();
-        
-        
         readyQueue=new ReadyQueue();    
-        
-        
-       
+   
         Dispatcher dispatcher = new Dispatcher();
         Generator generator = new Generator();
         
